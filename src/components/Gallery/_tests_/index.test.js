fb9c13a4b9dde6d3eee43c991ce9fc44aa71d4a7
Jennifer Kruk <jenkruk@gmail.com>
2021-03-22 09:36:25 -0700

    nav functionality and tests complete

---
+++ src/components/Gallery/_tests_/index.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Gallery from '..';
+
+afterEach(cleanup);
+
+const portrait = { name: "portraits", description: "Portraits of people in my life" };
+
+describe('Gallery is rendering', () => {
+
+    it('renders', () => {
+      render(<Gallery currentCategory={portrait} />);
+    });
+
+    // make sure the gallery matches its snapshot
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Gallery currentCategory={portrait} />)
+        expect(asFragment()).toMatchSnapshot()
+      })
+
+    //   make sure that the title of our <h1> is Portraits
+      it('renders', () => {
+        const { getByTestId } = render(<Gallery currentCategory={portrait} />)
+        expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
+      })
+  })
