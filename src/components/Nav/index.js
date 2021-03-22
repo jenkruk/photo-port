22625c94412fa1b8ea38295f58be998a4423f59f
Jennifer Kruk <jenkruk@gmail.com>
2021-03-22 07:44:36 -0700

    updated nav with hooks for active link functionality

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,30 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-const Nav = () => {
+function Nav(props) {
+  
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
 
-    const categories = [
-        {
-          name: "commercial",
-          description:
-            "Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        {
-          name: "landscape",
-          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-      ];
-
-      function categorySelected(name) {
-        console.log(`${name} clicked`)
-      }
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
     return (
     <header>
         <h2>
           <a data-testid="link" href="/">
-            <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+            <span role="img" aria-label="camera">📸</span>
+              Oh Snap!
           </a>
         </h2>
         <nav>
@@ -39,11 +33,14 @@ const Nav = () => {
             </li>
              {categories.map((category) => (
               <li
-                className="mx-1"
-                key={category.name}
-              >
-                <span onClick={() => categorySelected(category.name)}>
-                  {category.name}
+              className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+                }`} 
+                key={category.name}>
+                <span onClick={() => {
+                  setCurrentCategory(category)
+                }}>
+                  {capitalizeFirstLetter(category.name)}
                 </span>
               </li>
             ))}
