541e0071f200bd74bc386086efcb386ce7c308c4
Jennifer Kruk <jenkruk@gmail.com>
2021-03-22 22:41:59 -0700

    created modal for images

---
+++ src/components/Modal/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Modal({currentPhoto}) {
+    const {name, category, description, index} = currentPhoto;
+
+    return (
+    <div className="modalBackdrop">
+        <div className="modalContainer">
+            <h3 className="modalTitle">{name}</h3>
+            {/* // Had to add .default for images to show.  See reasoning here: https://stackoverflow.com/questions/43247696/javascript-require-vs-require-default */}
+            <img src={require(`../../assets/large/${category}/${index}.jpg`).default} alt="current category" />
+            <p>
+                {description}
+            </p>
+            <button type="button">
+            Close this modal
+            </button>
+        </div>
+    </div>
+    )
+}
+
+export default Modal;
